638cbcce2ffb6dab0fb8a26761ae0197e815a0c0
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-11-27 15:57:40 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
 
 console.log(galleryItems);
