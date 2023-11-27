e23524d478dc42ce6666da8cbc402bf6f8b5a28b
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-11-27 16:09:31 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (duration) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(duration));
+  }, 1000)
+);
+
+player
+  .setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+  )
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
