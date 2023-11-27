9d4a4dcce52f0c9a59096cb9e8ad08c5929e166f
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-11-27 16:00:55 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,5 +5,22 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
+const markUp = galleryItems.map(
+  ({ original, description, preview }) =>
+    `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `
+);
+
+gallery.innerHTML = markUp.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
